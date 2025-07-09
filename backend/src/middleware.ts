cd00f0c66b3294dd430c3015deccabb83cb43845
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-09 21:51:42 +0530

    added presigned url route

---
+++ backend/src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers['authorization'] ?? '';
+
+  try {
+    const decoded = jwt.verify(authHeader, process.env.JWT_SECRET!);
+
+    // @ts-ignore
+    if (decoded.userId) {
+      // @ts-ignore
+      req.userId = decoded.userId;
+      next();
+    } else {
+      res.send(403).json({
+        message: 'You are not logged in',
+      });
+    }
+  } catch (err) {
+    res.send(403).json({
+      message: 'You are not logged in',
+    });
+  }
+}
--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -1,9 +1,41 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
+import { authMiddleware } from '../middleware';
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
 
 const route = Router();
 const prismaClient = new PrismaClient();
+
+route.get('/presignedUrl', authMiddleware, async (req, res) => {
+  // @ts-ignore
+  const userId = req.userId;
+
+  const s3Client = new S3Client({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY!,
+      secretAccessKey: process.env.AWS_SECRET_KEY!,
+    },
+  });
+  const { url, fields } = await createPresignedPost(s3Client, {
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Key: `/fiver-uploads/${userId}/${Math.random()}/image.jpg`,
+    Conditions: [
+      ['content-length-range', 0, 10485760], // 10 MB
+    ],
+    Fields: {
+      'Content-Type': 'image/png',
+    },
+    Expires: 3600,
+  });
+
+  res.json({
+    presignedUrl: url,
+    fields,
+  });
+});
 
 //signin with wallet
 route.post('/signin', async (req, res) => {
