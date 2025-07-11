ecb0aba931803238dc2eb1ad729e3014732fe841
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-11 19:41:02 +0530

    updated navbar

--- client-frontend/components/Appbar.tsx
+++ client-frontend/components/Appbar.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Appbar = () => {
   return (
-    <div>
-      Hello
+    <div className='bg-blue-400 p-4'>
+      <div className='flex items-center justify-between'>
+        <span className='font-bold text-lg'>Dapp-Fiver</span>
+        <span>Connet Wallet</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appbar
+export default Appbar;
