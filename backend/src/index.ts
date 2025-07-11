3643e2c9b5509808ab0520f3faf04af0d2c71cc0
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-11 12:51:26 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import userRouter from './routers/user';
 import wokrerRouter from './routers/worker';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/v1/user', userRouter);
 app.use('/v1/worker', wokrerRouter);
