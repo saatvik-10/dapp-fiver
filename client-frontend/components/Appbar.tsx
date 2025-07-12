bf92418e95727d2f9471d4d9ad9c30ba3a10eb74
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-12 23:46:26 +0530

    updated appbar

--- client-frontend/components/Appbar.tsx
+++ client-frontend/components/Appbar.tsx
@@ -12,7 +12,7 @@ const Appbar = () => {
 
   return (
     <div className='bg-blue-400 p-4'>
-      <div className='flex items-center justify-between'>
+      <div className='flex items-center justify-between max-w-7xl mx-auto'>
         <span className='font-bold text-lg'>Dapp-Fiver</span>
         <button
           className='cursor-pointer bg-white p-1.5 text-blue-400 rounded-lg font-medium'
