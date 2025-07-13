bfdcb1ca317a719b830e4519c20740c2d2259925
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-13 17:48:38 +0530

    added security

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,11 +1,15 @@
 import z from 'zod';
 
 export const createTaskInput = z.object({
-  options: z.array(
-    z.object({
-      imageUrl: z.string(),
-    })
-  ),
+  options: z
+    .array(
+      z.object({
+        imageUrl: z.string(),
+      })
+    )
+    .min(2, {
+      message: 'At least 2 images are required',
+    }),
   title: z.string().optional(),
   signature: z.string(),
 });
