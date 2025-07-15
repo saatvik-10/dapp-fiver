17f712890ac8d049355b529f91d0e8baa7ea86df
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-15 16:02:02 +0530

    updated worked navbar

--- worker-frontend/components/Appbar.tsx
+++ worker-frontend/components/Appbar.tsx
@@ -1,27 +1,44 @@
 'use client';
 
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import {
+  WalletMultiButton,
+  WalletDisconnectButton,
+} from '@solana/wallet-adapter-react-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
 import { BACKEND_URL } from '@/utils/lib';
+import axios from 'axios';
 
 const Appbar = () => {
-  const signin = async () => {
-    const res = await axios.post(`${BACKEND_URL}/v1/worker/signin`, {});
+  const { publicKey, signMessage } = useWallet();
+
+  if (!publicKey) {
+    return;
+  }
+
+  async function handleSignMessage() {
+    const msg = new TextEncoder().encode(
+      `Welcome to D@pp-Fiv€₹!\n\nSign this message to authenticate your wallet and start earning by completing tasks.\n\nTimestamp: ${new Date().toISOString()}\nWallet: ${publicKey?.toString()}`
+    );
+    const signature = await signMessage?.(msg);
+
+    const res = await axios.post(`${BACKEND_URL}/v1/worker/signin`, {
+      signature,
+      publicKey: publicKey?.toString(),
+    });
+
     localStorage.setItem('token', res.data.token);
-  };
+  }
+
+  useEffect(() => {
+    handleSignMessage();
+  }, [publicKey]);
 
   return (
-    <div className='bg-purple-800 p-4'>
+    <div className='bg-white p-3 shadow-md'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
-        <span className='font-bold text-xl text-white'>
-          Dapp-Fiver <span className='text-xs'>(Worker)</span>
-        </span>
-        <button
-          className='cursor-pointer bg-white p-1.5 text-purple-800 rounded-lg font-medium'
-          onClick={signin}
-        >
-          Connect Wallet
-        </button>
+        <span className='font-bold text-xl text-purple-800'>D@pp-Fiv€₹</span>
+        {publicKey ? <WalletDisconnectButton /> : <WalletMultiButton />}
       </div>
     </div>
   );
